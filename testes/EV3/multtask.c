620197307459b6a2750718eaa8f88813fccf06dc
luanharaujo <luanharaujo@gmail.com>
2016-09-01 10:38:09 -0300

    testes para fazer mult task com o ev3

---
+++ testes/EV3/multtask.c
@@ -0,0 +1,6 @@
+task main()
+{
+	displayText(2,"3 segundos...");
+	wait(3,seconds);
+
+}
---
+++ testes/EV3/multtask2.c
@@ -0,0 +1,9 @@
+task main()
+{
+	displayText(1,"5 segundos...");
+	wait(5,seconds);
+
+	startTask(multtask, 10);
+
+
+}
